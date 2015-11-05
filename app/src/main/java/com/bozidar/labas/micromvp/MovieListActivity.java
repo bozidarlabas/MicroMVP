ebc01190540be6b303d71c3cc4eacafb5896d802
bozidar <bozidar.labas@hotmail.com>
2015-11-05 22:07:28 +0100

    Aded ButterKnife and cleared unnecessary methods from MovieListActivity

--- app/src/main/java/com/bozidar/labas/micromvp/MovieListActivity.java
+++ app/src/main/java/com/bozidar/labas/micromvp/MovieListActivity.java
@@ -2,31 +2,28 @@ package com.bozidar.labas.micromvp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MovieListActivity extends AppCompatActivity {
+
+    @Bind(R.id.recycler_view_movies)
+    RecyclerView recyclerViewMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies_list);
+        ButterKnife.bind(this);
+
+        initializeRecyclerView();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private void initializeRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerViewMovies.setLayoutManager(linearLayoutManager);
     }
 }
